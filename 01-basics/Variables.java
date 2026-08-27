824e3f770fea3c43254b3236a11a582791c41f62
Ajit Nishad <ajitnishad36@gmail.com>
2026-08-27 16:59:47 +0530

    Add Variables class with main method

---
+++ 01-basics/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int age = 25;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isDeveloper = true;
+        String name = "Ajit Nishad";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Developer: " + isDeveloper);
+    }
+}
