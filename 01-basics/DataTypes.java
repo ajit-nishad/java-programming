3575f63fe8f401db1144204c208563e4ed52a46e
Ajit Nishad <ajitnishad36@gmail.com>
2026-08-27 17:01:17 +0530

    Add DataTypes class with primitive data types example

---
+++ 01-basics/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Primitive data types
+
+        byte byteValue = 100;
+        short shortValue = 10000;
+        int intValue = 50000;
+        long longValue = 100000L;
+
+        float floatValue = 10.5f;
+        double doubleValue = 99.99;
+
+        char character = 'A';
+        boolean status = true;
+
+        System.out.println("Byte: " + byteValue);
+        System.out.println("Short: " + shortValue);
+        System.out.println("Int: " + intValue);
+        System.out.println("Long: " + longValue);
+        System.out.println("Float: " + floatValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Character: " + character);
+        System.out.println("Boolean: " + status);
+    }
+}
