91c27584a979a25f13c0a6c73e73a7b0d4148985
Ajit Nishad <ajitnishad36@gmail.com>
2026-08-27 17:08:20 +0530

    Add Do-While loop example in Do_While_Loop.java

---
+++ 01-basics/Do_While_Loop.java
@@ -0,0 +1,12 @@
+public class Do_While_Loop {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 10);
+    }
+}
